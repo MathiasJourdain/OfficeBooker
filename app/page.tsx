a4321b2ed6b297c69658d281b20f1b09c191a242
MathiasJourdain <Mathias.Jourdain@etu.univ-littoral.fr>
2026-01-15 15:14:36 +0100

    modif

--- app/page.tsx
+++ app/page.tsx
@@ -19,7 +19,7 @@ export default async function Home({
       {errorMessage && <ErrorPopup message={errorMessage} />}
 
       <div className="max-w-6xl mx-auto p-8">
-        <HomeHeader />
+        <HomeHeader/>
         
         <section className="mb-8">
             <NewsFeed />
